81dcd770b7bdd65d585f6d1a661be3fa57b0cb27
jpimagestudio <raga_05lard@icloud.com>
2024-09-04 13:45:31 +0800

    v12
    change hotspot for more space between them.

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 7.1.3/20932
 // Filename: skin_with_FloorPlan_and_Logo_finsih_phonescreen.ggsk
-// Generated 2024-09-04T13:24:20
+// Generated 2024-09-04T13:44:35
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_thumbnail_menu', 2, false, { ignoreInState: 1  });
